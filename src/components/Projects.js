24d016504e77be478f2c57af9e1adc678dd2f5e6
Alex Bachicha <alexandra.bachicha@hotmail.com>
2021-03-12 19:45:45 -0800

    created React app with components

---
+++ src/components/Projects.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PRojects extends Component {
+    render() {
+        return (
+            <div><h1>Projects Page</h1></div>
+        )
+    }
+}
+
+export default Projects;
