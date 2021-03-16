f09f95453acdf5f522633bcba58b3c399b9e6b83
Alex Bachicha <alexandra.bachicha@hotmail.com>
2021-03-15 19:58:11 -0700

    added 3/4 social media links

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,34 +7,30 @@ class Landing extends Component {
             <div style={{width: '100%', margin: 'auto'}}>
                 <Grid className="landing-grid">
                     <Cell col={12}> 
-                        <img
-                            src="https://lh3.googleusercontent.com/znz9d_kpK4XKpf5K2T-z3ZCUOFKBFkHkvDG66Zn_cwZFdNA6Co1YBJ00M-SYqqxuQTIViws=s97"
-                            alt="bitmoji"
-                            className="bitmoji-img"
-                        />
+                        <img src="https://lh3.googleusercontent.com/znz9d_kpK4XKpf5K2T-z3ZCUOFKBFkHkvDG66Zn_cwZFdNA6Co1YBJ00M-SYqqxuQTIViws=s97" alt="bitmoji" className="bitmoji-img"/>
 
                         <div className="banner-text">
                             <h1>Full Stack Web Developer</h1>
-
+                            
                             <hr/>
 
                             <p>HTML | CSS | Bootstrap | JavaScript | React | NodeJS | Express | MongoDB</p>
 
                             <div className="social-links">
                                 {/* LinkedIn */}
-                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                <a href="https://www.linkedin.com/in/alex-bachicha-a10a6015b/" rel="noopener noreferrer" target="_blank">
                                     <i className="fa fa-linkedin-square" aria-hidden="true" />
                                 </a>
                                 {/* GitHub */}
-                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                <a href="https://github.com/alexbachicha" rel="noopener noreferrer" target="_blank">
                                     <i className="fa fa-github-square" aria-hidden="true" />
                                 </a>
                                 {/* FreeCodeCamp */}
-                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-                                    <i className="fa fa-free-code-camp" aria-hidden="true" />
+                                <a href="https://www.instagram.com/cheecchx/" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-instagram" aria-hidden="true" />
                                 </a>
                                 {/* YouTube */}
-                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                <a href="http://youtube.com" rel="noopener noreferrer" target="_blank">
                                     <i className="fa fa-youtube-square" aria-hidden="true" />
                                 </a>
                             </div>
