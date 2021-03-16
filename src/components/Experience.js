96ad8eafec86c03a26e4188b25d86f8c04a9ddf7
Alex Bachicha <alexandra.bachicha@hotmail.com>
2021-03-15 19:50:37 -0700

    added spacing to help visualize code

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,6 +8,7 @@ class Experience extends Component {
                 <Cell col={4}>
                     <p>{this.props.startYear} - {this.props.endYear}</p>
                 </Cell>
+                
                 <Cell col={8}>
                     <h4 style={{marginTop: '0px'}}>{this.props.jobName}</h4>
                     <p>{this.props.jobDescription}</p>
